eb64b95c1b1b180e0d7329b08f47b2394dba163c
gauravi <gauravi.girase@gmail.com>
2022-12-20 01:31:55 +0530

    Completed Age screen

--- app/screens/Age.js
+++ app/screens/Age.js
@@ -1,14 +1,45 @@
-import React from 'react';
-import { Pressable, View, Text, Image } from 'react-native';
+import React, {useState} from 'react';
+import { Pressable, View, Text, Image, TextInput, Button } from 'react-native';
+import DateTimePicker from '@react-native-community/datetimepicker';
 import CommonStlye from '../styles/CommonStlye';
+import AgeStyle from '../styles/AgeStyle';
 
 const Age = ({navigation, route}) => {
+    const [age, setAge] = useState("Age");
+    const [isDisplayDate, setShow] = useState(false);
+
+    const changeSelectedDate = (event, selectedDate) => {
+        setShow(false);
+        const currentDate = selectedDate;
+        setAge(`${currentDate.getDate()}-${currentDate.getMonth()}-${currentDate.getFullYear()}`);
+    };
+
+    const displayDatepicker = () => {
+      setShow(true);
+    };
+
     return (
       <View style={CommonStlye.container}>
         <Text style={CommonStlye.heading}>How old are you?</Text>
+        
         <Text style={CommonStlye.content}>
           Your age helps us to create a tailored guidance according to your age group.
         </Text>
+
+        <Pressable style={AgeStyle.inputContainer} onPress={displayDatepicker}>
+          <Image source={require('../assets/img/calender-Icon.png')} />
+          <TextInput style={AgeStyle.inputs} editable={false} value={age}/>
+        </Pressable>
+
+        {isDisplayDate && (
+            <DateTimePicker
+                value={new Date()}
+                mode='date'
+                display="default"
+                dateFormat='DD-MM-YYYY'
+                onChange={changeSelectedDate}
+            />
+        )}
 
         <Pressable style={CommonStlye.arrowBtn} 
           onPress={() => {navigation.navigate("Explore")}}>
---
+++ app/styles/AgeStyle.js
@@ -0,0 +1,20 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  inputs:{
+    position: 'absolute',
+    paddingTop: '2.4%',
+    paddingLeft: '18%',
+    color: '#00B5F8',
+  },
+  inputContainer: {
+    borderColor:'#95DAD7',
+    borderWidth:0.5, 
+    borderRadius:5,  
+    backgroundColor:'#DBECEF',
+    height: '5%',
+    width:'76%',
+    paddingTop: '2%',
+    paddingLeft: '2%'
+  },
+});
