724864247618ba215f29f0b81f5453ab28d09518
Nits PC <nitinrathod005@gmail.com>
2023-01-11 10:34:10 +0530

    added tailwind-css library

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 import React from 'react';
-
+import { TailwindProvider } from 'tailwindcss-react-native';
 import Routes from './app/Routes';
 
 export default function App() {
   return (
-    < Routes />
+    <TailwindProvider>
+      < Routes />
+    </TailwindProvider>
   );
 }
--- app/screens/Explore.js
+++ app/screens/Explore.js
@@ -1,35 +1,37 @@
 import React from 'react';
-import { View, Text, Image, Pressable } from 'react-native';
+import { View, Text, Image, Pressable, SafeAreaView } from 'react-native';
 import ExploreStyle from '../styles/ExploreStyle';
 import CommonStlye from '../styles/CommonStlye';
 
 const Explore = ( {navigation} ) => {
     return (
-      <View style={CommonStlye.container}>
-        <Text style={ExploreStyle.appName}>
-          Corner
-          <Text style={{color: '#00B5F8'}}>stone</Text>
-        </Text>
-
-        <View style={ExploreStyle.logoCircle}>
-          <Image style={ExploreStyle.logo} source={require('../assets/img/logo.png')} />
-        </View>
-
-        <Text style={ExploreStyle.text}>
-          Every person is unique and require mental health support as needed.
-        </Text>
-
-        <Pressable style={CommonStlye.button} 
-          onPress={() => {navigation.navigate("Nickname")}}>
-          <Text style={CommonStlye.buttonText}>
-            Start Exploring
+      <SafeAreaView >
+        <View style={CommonStlye.container}>
+          <Text style={ExploreStyle.appName}>
+            Corner
+            <Text style={{color: '#00B5F8'}}>stone</Text>
           </Text>
-        </Pressable>
 
-        <Text style={ExploreStyle.smallText}>
-          Create an account?  SignUp
-        </Text>
-      </View>
+          <View style={ExploreStyle.logoCircle}>
+            <Image style={ExploreStyle.logo} source={require('../assets/img/logo.png')} />
+          </View>
+
+          <Text style={ExploreStyle.text}>
+            Every person is unique and require mental health support as needed.
+          </Text>
+
+          <Pressable style={CommonStlye.button} 
+            onPress={() => {navigation.navigate("Nickname")}}>
+            <Text style={CommonStlye.buttonText}>
+              Start Exploring
+            </Text>
+          </Pressable>
+
+          <Text style={ExploreStyle.smallText}>
+            Create an account?  SignUp
+          </Text>
+        </View>
+      </SafeAreaView>
     )
 };
 
