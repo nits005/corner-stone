a6d8995221d3d93ae39d4f2a9b77a4d7a619be0f
Nits PC <nitinrathod005@gmail.com>
2023-01-11 12:10:49 +0530

    Explore page with tailwind css

--- app/screens/Explore.js
+++ app/screens/Explore.js
@@ -1,36 +1,45 @@
 import React from 'react';
 import { View, Text, Image, Pressable, SafeAreaView } from 'react-native';
-import ExploreStyle from '../styles/ExploreStyle';
 import CommonStlye from '../styles/CommonStlye';
 
 const Explore = ( {navigation} ) => {
     return (
-      <SafeAreaView >
-        <View style={CommonStlye.container}>
-          <Text style={ExploreStyle.appName}>
-            Corner
-            <Text style={{color: '#00B5F8'}}>stone</Text>
-          </Text>
-
-          <View style={ExploreStyle.logoCircle}>
-            <Image style={ExploreStyle.logo} source={require('../assets/img/logo.png')} />
-          </View>
-
-          <Text style={ExploreStyle.text}>
-            Every person is unique and require mental health support as needed.
-          </Text>
-
-          <Pressable style={CommonStlye.button} 
-            onPress={() => {navigation.navigate("Nickname")}}>
-            <Text style={CommonStlye.buttonText}>
-              Start Exploring
-            </Text>
-          </Pressable>
-
-          <Text style={ExploreStyle.smallText}>
-            Create an account?  SignUp
-          </Text>
+      <SafeAreaView className="bg-white items-center justify-center flex-1">
+        {/* app name */}
+        <View className="flex-row">
+          <Text className="text-blue-800 text-5xl">Corner</Text>
+          <Text className="text-sky-400 text-5xl">stone</Text>
         </View>
+
+        {/* app logo */}
+        <View className="p-14">
+          <View className="border border-gray-600 rounded-full p-5">
+            <Image source={require('../assets/img/logo.png')}
+              className="h-40 w-40 rounded-full"
+            />
+          </View>
+        </View>
+
+
+        {/* slogan */}
+        <Text className="text-3xl text-gray-500 text-center px-4">
+          Every person is unique and require mental health support as needed.
+        </Text>
+
+        {/* start explore button */}
+        <View className="p-14">
+          <Pressable className="bg-sky-500 p-3 w-80"
+              onPress={() => {navigation.navigate("Nickname")}}>
+              <Text style={CommonStlye.buttonText}>
+                Start Exploring
+              </Text>
+          </Pressable>
+        </View>
+
+        {/* sign up option */}
+        <Text className="text-base">
+          Create an account?  SignUp
+        </Text>
       </SafeAreaView>
     )
 };
